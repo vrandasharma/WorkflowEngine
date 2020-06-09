1f314e41218d662b8d20ee87d6e543a61daac6bd
vranda <vranda.sharma@gmail.com>
2020-06-09 10:37:55 +0530

    Redundant files removed

--- src/main/java/com/workflow_runner/sales_workflow/service/IWorkflowRunnerInterface.java
+++
@@ -1,9 +0,0 @@
-package com.workflow_runner.sales_workflow.service;
-
-import com.workflow_runner.sales_workflow.dto.WorkflowRawRequestDTO;
-import com.workflow_runner.sales_workflow.dto.WorkflowStepsInfoDTO;
-
-public interface IWorkflowRunnerInterface {
-
-	void executeWorkflowSteps(WorkflowRawRequestDTO wfrawRequestModel);
-}
